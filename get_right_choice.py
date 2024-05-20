3a3060eb00ab4e2e3098a04dbed679f78cba12cc
vinhnguyen2110 <57150520+quangvinh2110@users.noreply.github.com>
2024-05-20 11:12:38 +0700

    Update get_right_choice.py

--- get_right_choice.py
+++ get_right_choice.py
@@ -11,6 +11,7 @@ from transformers import (
 from peft import PeftModel
 from datasets import load_from_disk
 from src.infer.base_pipeline import BasePipeline
+from src.infer.code_pipeline import CodePipeline
 from tqdm import tqdm
 
 MODEL_LIST = [
@@ -34,12 +35,29 @@ def read_jsonl(filepath: str):
             data.append(json.loads(line))
     return data
 
+def get_model_name_from_adapter_name(adapter_name: str) -> str:
+    for model_name in MODEL_LIST:
+        if model_name in adapter_name:
+            return model_name
+
 def parse_args():
     parser = argparse.ArgumentParser(description="")
     parser.add_argument(
-        "--peft_model_name",
+        "--pipeline",
         type=str,
-        default=None,
+        default="base",
+        help=""
+    )
+    parser.add_argument(
+        "--model_name",
+        type=str,
+        default="",
+        help=""
+    )
+    parser.add_argument(
+        "--adapter_name",
+        type=str,
+        default="",
         help=""
     )
     parser.add_argument(
@@ -66,68 +84,67 @@ def parse_args():
     args = parser.parse_args()
 
     # Sanity checks
-    if not (args.peft_model_name and args.dataset_name):
+    if not (args.adapter_name or args.model_name) or not args.dataset_name:
+        raise ValueError("")
+    if args.use_adapter and not args.adapter_name:
         raise ValueError("")
     
     return args
 
+
+def prepare_pipeline(args):
+    Pipeline = BasePipeline if args.pipeline=="base" else CodePipeline
+    pipeline = Pipeline(
+        model_path=args.model_path,
+        quantize="4bit" if args.do_quantize else None,
+        adapter_path=args.adapter_path if args.use_adapter else None,
+        merge_adapter=args.merge_adapter,
+    )
+    return pipeline
+
+
 if __name__ == "__main__":
 
-    print("="*61)
-    print("="*20+" VIMATHQA GET ANSWER "+"="*20)
-    print("="*61)
+    print("="*63)
+    print("="*20+" VIMATHQA RIGHT CHOICE "+"="*20)
+    print("="*63)
     args = parse_args()
-    models_hub = "/workspace/home/vinhnq29/zac2023-main/models_hub/"
-    model_path = ""
-    peft_models_hub = "/workspace/home/vinhnq29/zac2023-main/checkpoints/adapters/"
-    peft_model_name = args.peft_model_name
-    peft_model_path = peft_models_hub + peft_model_name
-    for model_name in MODEL_LIST:
-        if model_name in peft_model_name:
-            model_path = models_hub + model_name
+    base_models_hub = "/workspace/home/vinhnq29/zac2023-main/models_hub/"
+    merged_models_hub = "/workspace/home/vinhnq29/zac2023-main/checkpoints/full_models/"
+    adapters_hub = "/workspace/home/vinhnq29/zac2023-main/checkpoints/adapters/"
+    # args.adapter_path = adapters_hub + args.adapter_name
+    if "lora" in args.model_name or "qlora" in args.model_name:
+        args.model_path = merged_models_hub + args.model_name
+        args.adapter_name = ""
+        args.adapter_path = ""
+        args.use_adapter = False
+        args.merge_adapter = False
+    elif args.adapter_name:
+        args.model_name = get_model_name_from_adapter_name(args.adapter_name)
+        args.model_path = base_models_hub + args.model_name
+        args.adapter_path = adapters_hub + args.adapter_name
+    else:
+        args.model_path = base_models_hub + args.model_name
+        args.adapter_name = ""
+        args.adapter_path = ""
+        args.use_adapter = False
+        args.merge_adapter = False
     dataset_name = args.dataset_name
     result_folder_hub = "/workspace/home/vinhnq29/zac2023-main/results/"
-    result_folder_path = result_folder_hub+normalize_name(peft_model_name)
+    if args.adapter_name:
+        result_folder_path = result_folder_hub+normalize_name(args.adapter_name)
+    else: 
+        result_folder_path = result_folder_hub+normalize_name(args.model_name)
     result_filepath = result_folder_path+"/"+normalize_name(dataset_name)+".jsonl"
-    print(f"MODEL PATH: {model_path}")
-    print(f"PEFT MODEL PATH: {peft_model_path}")
+    print(f"MODEL PATH: {args.model_path}")
+    print(f"ADAPTER PATH: {args.adapter_path}")
     print(f"RESULT PATH: {result_filepath}")
     print(f"DO QUANTIZE: {args.do_quantize}")
     print(f"USE ADAPTER: {args.use_adapter}")
     print(f"MERGE ADAPTER: {args.merge_adapter}")
-    print("="*61)
+    print("="*63)
 
-    if args.do_quantize:
-        bnb_config=BitsAndBytesConfig(
-            load_in_4bit=True,
-            bnb_4bit_compute_dtype=torch.bfloat16,
-            bnb_4bit_use_double_quant=True,
-            bnb_4bit_quant_type="nf4",
-        )
-        model = AutoModelForCausalLM.from_pretrained(  
-            model_path, 
-            torch_dtype=torch.bfloat16,
-            quantization_config=bnb_config, 
-            # load_in_4bit=True,
-            trust_remote_code=True, 
-            device_map="auto"
-        )
-    else:
-        model = AutoModelForCausalLM.from_pretrained(  
-            model_path, 
-            torch_dtype=torch.bfloat16,
-            trust_remote_code=True, 
-            device_map="auto"
-        )
-    if args.use_adapter:
-        model = PeftModel.from_pretrained(model, peft_model_path)
-    if args.use_adapter and args.merge_adapter:
-        model = model.merge_and_unload()
-    tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
-    pipeline = BasePipeline(
-        model=model,
-        tokenizer=tokenizer
-    )
+    pipeline = prepare_pipeline(args)
     
     infer_result = read_jsonl(result_filepath)
     output_file = open(result_folder_path+"/"+normalize_name(dataset_name)+"-pred_choice.jsonl", "w")
