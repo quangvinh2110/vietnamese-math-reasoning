d63b3d583eaab9f986646afd63285472f4345879
vinhnguyen2110 <57150520+quangvinh2110@users.noreply.github.com>
2024-06-20 20:22:39 +0700

    Delete scripts/format_input_output_instruct_data.sh

--- scripts/format_input_output_instruct_data.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash#
-set -ex
-
-
-python src/utils/format_input_output_instruct_data.py \
---model_name your_model_name \
---input_file /path/to/output/file \
---output_file /path/to/output/file \
