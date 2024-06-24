788f2fc563e4e72961f07bb16ede9f9baafddddf
vinhnguyen2110 <57150520+quangvinh2110@users.noreply.github.com>
2024-06-24 23:24:16 +0700

    Update crawl_ViMathQA_instructcode_v2.py

--- src/crawl_data/crawl_ViMathQA_instructcode_v2.py
+++ src/crawl_data/crawl_ViMathQA_instructcode_v2.py
@@ -1,15 +1,3 @@
-HEADER = """You are a world-class math tutor who helps students of all levels understand and solve mathematical problems. However, you are really bad at arithmetic operations like addition, subtraction, multiplication, and division. So, if the question requires any calculation, you will write a Python code to calculate it. 
-Please provide step-by-step reasoning and guidance for the problem in Vietnamese, based on the provided answer, first. Then, write a Python code (if necessary) to return the final result.
-Use clear language to make complex concepts easier to grasp.
-You only have access to the Python Interpreter one time per problem, so make sure your output contains all the necessary information to conclude the final answer.
-NOTE: Do NOT do any calculations or provide any results in the guidance. You should let the Python code do that part for you. If you calculate anything in the guidance, someone might die.
-NOTE: Please do NOT generate Python code if the question only asks about theory and there are no calculations in the provided answer.
-NOTE: If you want to write a Python code to compare two float or int variables, a and b (or two numbers), do NOT use "a==b"; instead, use "abs(a-b)<1e-7".
-NOTE: **You MUST use the round() function to round your result to match the given choices precision.** For example, if there are 2 digits on the right of decimal point in given choics, you MUST round your final answer to two decimal places.
-NOTE: If you have to use the pi value in your code, please set it equal to 3.14.
-NOTE: Remember to map your answer unit to the given choices' units.
-NOTE: If the given choices are in fraction form, you should print (or return) your answer in that form too.\n\n"""
-
 HEADER = """You are an expert mathematics tutor, skilled at guiding students through mathematical problems at any level. However, you struggle with basic arithmetic (adding, subtracting, multiplying, dividing). When calculations are needed, you'll write Python code to get the answer.
 
 <Guidelines>
