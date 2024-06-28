08836c582207d43a011c85ee80b9e787c986a67d
vinhnguyen2110 <57150520+quangvinh2110@users.noreply.github.com>
2024-06-28 19:59:22 +0700

    Update format_input_output_data.sh

--- scripts/format_input_output_data.sh
+++ scripts/format_input_output_data.sh
@@ -3,14 +3,14 @@ set -ex
 
 
 # python src/utils/format_input_output_instruct.py \
-# --model_name vistral-7b-chat \
+# --model_name deepseek-math-7b-rl \
 # --input_file /workspace/home/vinhnq29/zac2023-main/data_hub/ViMathQA/train_v2/train_2.jsonl \
-# --output_file /workspace/home/vinhnq29/zac2023-main/data_hub/ViMathQA/train_v2/input_output_vistral.json \
+# --output_file /workspace/home/vinhnq29/zac2023-main/data_hub/ViMathQA/train_v2/input_output_dsmath.json \
 # # --eos_token "<|eot_id|>"
 
 
 python src/utils/format_input_output_instructcode.py \
---model_name qwen2-7b-instruct \
---input_file /workspace/home/vinhnq29/zac2023-main/data_hub/ViMathIntructCode/instructcode_vimathqa.jsonl \
---output_file /workspace/home/vinhnq29/zac2023-main/data_hub/ViMathIntructCode/input_output_qwen.json \
-# --eos_token "<|eot_id|>"
+--model_name Llama-3-8B-Instruct \
+--input_file /workspace/home/vinhnq29/zac2023-main/data_hub/ViMathIntructCode/train-2024-06-27-27-01-15/vimathqa_llama_qwen.jsonl \
+--output_file /workspace/home/vinhnq29/zac2023-main/data_hub/ViMathIntructCode/train-2024-06-27-27-01-15/input_output_llama3.json \
+--eos_token "<|eot_id|>"
